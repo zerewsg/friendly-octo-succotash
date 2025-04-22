7c4eb2520b8383384444787293b0976388d18dad
zerewsg <ger62076309kyc@gmail.com>
2025-04-22 20:37:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// const originalArray = [1, 2, 3, 4, 5];
+// const newArray = originalArray.map(number => number * 2);
+// console.log(newArray); 
+
+
+
+// const celsiusTemps = [0, 10, 20, 30];
+// const fahrenheitTemps = celsiusTemps.map(c => (c * 9/5) + 32);
+// console.log(fahrenheitTemps); 
+
+
+
+// const numbers = [3, 5, 7];
+// const squaredNumbers = numbers.map(num => num ** 2);
+// console.log(squaredNumbers); 
+
+
+
+// const numbers = [10, 20, 30];
+// const strings = numbers.map(num => String(num)); 
+// console.log(strings); 
